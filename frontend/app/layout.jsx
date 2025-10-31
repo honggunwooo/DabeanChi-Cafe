b978ce455a46b755ce648e5cdde20e590805b4b6
carbonara6101 <jungho9966@gmail.com>
2025-11-01 01:39:43 +0900

    으라차차

--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -2,7 +2,7 @@ import { Analytics } from "@vercel/analytics/next"
 import "./styles/globals.css"
 
 export const metadata = {
-  title: "박태환 바보",
+  title: "박태환",
   description: "최고 품질의 커피 원두를 만나보세요",
   // generator: "v0.app",
 }
